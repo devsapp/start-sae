187d13e52fcd8b57f25c57c4cb51294c615983d6
pichunying <2339348937@qq.com>
2022-07-22 16:25:37 +0800

    fix: add php zip

---
+++ start-sae-php-zip/src/code/php/index.php
@@ -0,0 +1,55 @@
+<?php
+header('Content-Type: text/html;charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <title>第一个 SAE 应用</title>
+    <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<br><br>
+
+<div style="padding-left:30px; padding-right:30px">
+    <span style='font-size:18px;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;你好，SAE 用户。恭喜你成功运行了第一个应用！</span><br><br>
+    <span style='font-size:14px;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SAE 是一个围绕应用和微服务的PaaS平台，提供多样的应用发布能力和轻量级微服务解决方案，帮助用户解决在应用和服务管理过程中监控、诊断和高可用运维问题；其提供了开源软件Dubbo的商业版。</span><br>
+
+    <span style='font-size:14px;'><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;关键字：应用运维、微服务、Dubbo、SpringBoot、PHP、鹰眼监控、服务治理</b></span><br><br>
+
+    <span style='font-size:14px;'>官网：<a href='https://www.aliyun.com/product/sae' target='_blank'>https://www.aliyun.com/product/sae</a> </span><br>
+    <span style='font-size:14px;'>快速入门：<a href='https://help.aliyun.com/document_detail/120281.html' target='_blank'>https://help.aliyun.com/document_detail/120281.html</a> </span><br>
+    <br>
+    <div align="right" style='font-size:14px;'>
+        SAE 项目组<br>
+        2019 年
+    </div>
+</div>
+
+<br><br><br>
+
+<div style="padding-left:30px; padding-right:30px">
+    <span style='font-size:18px;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Hello, SAE User. Congratulations! You have just deployed your first application using SAE!  </span><br><br>
+
+    <span style='font-size:14px;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SAE is a PaaS platform designed to host applications and micro-services. It provides a variety of application deploying methods and lightweight micro-service solutions, helps users to monitor and diagnose applications and services during the whole lifecycle, and at the same time fully addresses highly availability requirements in terms of platform maintenance. SAE provides the commercial edition of open source software Dubbo.</span><br>
+
+    <span style='font-size:14px;'><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Keywords: Application operation, Micro-service, Dubbo, SpringBoot, PHP, EagleEye monitoring, Micro-service management</b></span><br><br>
+
+    <span style='font-size:14px;'>Website：<a href='https://www.aliyun.com/product/sae' target='_blank'>https://www.aliyun.com/product/sae</a> </span><br>
+    <span style='font-size:14px;'>Quick start：<a href='https://help.aliyun.com/document_detail/120281.html'
+                                                 target='_blank'>https://help.aliyun.com/document_detail/120281.html</a> </span><br>
+
+    <div align="right" style='font-size:14px;'>
+        SAE Team<br>
+        2019Y
+    </div>
+</div>
+
+<div align="center" style='font-size:14px;'>
+    PHP: <?php echo phpversion();?>
+</div>
+
+</body>
+</html>
