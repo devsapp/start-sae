959f282cbe8cc197db4fa09943b5f495312eff40
heimanba <yuankun.yk@alibaba-inc.com>
2022-11-16 21:29:49 +0800

    add publish

--- start-sae-java-jar/src/code/src/test/java/com/example/demo/DemoApplicationTests.java
+++ start-sae-java-jar/src/code/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+	@Test
+	public void testSayHello() {
+	}
 }
